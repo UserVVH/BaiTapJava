220742fdd6bba2fa7751d4ce842b52533abaed64
UserVVH <58172204+UserVVH@users.noreply.github.com>
2024-03-25 20:12:14 +0700

    Update Bai1.java

--- Buoi4/Bai1.java
+++ Buoi4/Bai1.java
@@ -24,7 +24,7 @@ public class DemoBuoi4 {
 				}
 				break;
 			} catch (NumberFormatException e) {
-				System.out.println("bạn phải nhập vào số nguyên, ví dụ: 2001");
+				System.out.println("Bạn phải nhập vào số nguyên, ví dụ: 2001");
 			}
 		}
 		return namSinh;
