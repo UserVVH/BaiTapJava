67ca9ba3a5f7f2b7289b9a0bb3201cdad3ff5807
UserVVH <58172204+UserVVH@users.noreply.github.com>
2024-03-29 15:17:34 +0700

    Create Bai6.java

---
+++ Buoi5/Bai6.java
@@ -0,0 +1,53 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void inSoLeChanWhile() {
+		int i = 0;
+		System.out.println("Các số nguyên dương lẻ nhỏ hơn 100:");
+		while (i < 100) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+			++i;
+		}
+
+		i = 0;
+		System.out.println("\nCác số nguyên dương chẵn nhỏ hơn 100:");
+		while (i < 100) {
+			if (i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+			++i;
+		}
+	}
+
+	public static void inSoLeChanFor() {
+		System.out.println("Các số nguyên dương lẻ nhỏ hơn 100:");
+		for (int i = 0; i < 100; ++i) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+
+		System.out.println("\nCác số nguyên dương chẵn nhỏ hơn 100:");
+		for (int i = 0; i < 100; ++i) {
+			if (i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Sử dụng vòng lặp while:");
+		inSoLeChanWhile();
+		System.out.println("\nSử dụng vòng lặp for:");
+		inSoLeChanFor();
+		in.close();
+
+	}
+
+}
