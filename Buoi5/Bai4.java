014628d7139420dca74d5c425c33531a4537f7aa
UserVVH <58172204+UserVVH@users.noreply.github.com>
2024-03-29 14:45:31 +0700

    Update Bai4.java

--- Buoi5/Bai4.java
+++ Buoi5/Bai4.java
@@ -2,23 +2,25 @@ package hello;
 
 import java.util.Scanner;
 
-public class DemoBuoi6 {
+public class Main {
+
+	private static int tinhTong(int x, int n) {
+		int tong = 0;
+		for (int i = 1; i <= n; i++) {
+			tong += Math.pow(x, i);
+		}
+		return tong;
+	}
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-
-		System.out.print("Nhập giá trị của x: ");
-		double x = in.nextDouble();
-
-		System.out.print("Nhập giá trị của n: ");
+		System.out.print("Nhập vào số nguyên n: ");
 		int n = in.nextInt();
+		System.out.print("Nhập vào số nguyên x: ");
+		int x = in.nextInt();
 
-		double sum = 0;
-		for (int i = 1; i <= n; i++) {
-			sum += Math.pow(x, i);
-		}
-
-		System.out.printf("Tổng của dãy số S(n) là: %.2f", sum);
+		int tong = tinhTong(x, n);
+		System.out.println("Tổng S(n) = " + tong);
 		in.close();
 
 	}
