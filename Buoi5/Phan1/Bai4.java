d00464df2abd8b93d900e7afa66ac9f0c2e4a942
UserVVH <58172204+UserVVH@users.noreply.github.com>
2024-03-29 16:06:41 +0700

    Create Bai4.java

---
+++ Buoi5/Phan1/Bai4.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static int tinhTong(int x, int n) {
+		int tong = 0;
+		for (int i = 1; i <= n; i++) {
+			tong += Math.pow(x, i);
+		}
+		return tong;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Nhập vào số nguyên n: ");
+		int n = in.nextInt();
+		System.out.print("Nhập vào số nguyên x: ");
+		int x = in.nextInt();
+
+		int tong = tinhTong(x, n);
+		System.out.println("Tổng S(n) = " + tong);
+		in.close();
+
+	}
+
+}
