66d34a66eecd692b8bc7d8b2eb48844467a9354d
UserVVH <58172204+UserVVH@users.noreply.github.com>
2024-03-30 08:10:47 +0700

    Update Bai1.java

--- Buoi6/Bai1.java
+++ Buoi6/Bai1.java
@@ -1,1 +1,30 @@
+package hello;
 
+import java.util.Scanner;
+
+public class Main {
+	public static int timMax(int a, int b, int c) {
+		int max = a;
+		if (max < b) {
+			max = b;
+		}
+		if (max < c) {
+			max = c;
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Nhập a: ");
+		int a = in.nextInt();
+		System.out.print("Nhập b: ");
+		int b = in.nextInt();
+		System.out.print("Nhập c: ");
+		int c = in.nextInt();
+		System.out.printf("Số lớn nhất trong 3 số %d %d %d là: %d", a, b, c, timMax(a, b, c));
+		in.close();
+
+	}
+
+}
