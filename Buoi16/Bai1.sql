1eb408722d938698f510fac548821ac6437f1271
UserVVH <58172204+UserVVH@users.noreply.github.com>
2024-05-14 19:25:55 +0700

    Update Bai1.sql

--- Buoi16/Bai1.sql
+++ Buoi16/Bai1.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Students (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Id INT AUTO_INCREMENT,
     Full_name VARCHAR(255) NOT NULL,
     Gender VARCHAR(10),
     Age INT,
     City VARCHAR(100),
-    Weight DECIMAL(5,2)
+    Weight DECIMAL(5,2),
+    PRIMARY KEY (Id)
 );
